e2f79f203d7e9cb6c2c668fab1241fde70ce6444
Dabaou-iiazaar <48531073+Dabaou-iiazaar@users.noreply.github.com>
2020-05-30 13:58:39 -0400

    Add files via upload

---
+++ MainG.java
@@ -0,0 +1,14 @@
+import java.util.*;//Importing for graphics and other helpful additions.
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.image.*;  
+import javax.imageio.*; 
+import javax.sound.midi.*;
+import java.applet.*;
+import java.lang.Math;
+import javax.swing.Timer;//Specifying which Timer since there would be a conflict with util otherwise.
+public class MainG extends JFrame{
+  
+}
