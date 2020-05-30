3d02d8e59c932113af2b325d890f11ba799a0096
Neekirby <38818338+Leafkirby6607@users.noreply.github.com>
2020-05-30 14:18:08 -0400

    Add files via upload
    Sheep Class

---
+++ Sheep.java
@@ -0,0 +1,27 @@
+//IMPORTING
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.imageio.*;
+import java.awt.image.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.awt.Rectangle;
+import java.awt.Image;
+import java.io.*;
+
+public class Sheep {
+	
+	private int x, y, vx, vy;
+	
+    public Sheep(int X, Y) {
+    	x=X;
+    	y=Y;
+    }
+    
+    public Rectangle SheepBox(){
+    	return new Rectangle(x, y, 20, 20); //can be changed accordingly 
+    }
+    
+}
