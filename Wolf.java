78a84e8fc837f5f416ee428b64b12cdc41e31f87
andy-liuu <59021924+andy-liuu@users.noreply.github.com>
2020-05-30 23:04:05 -0400

    Update Wolf.java

--- Wolf.java
+++ Wolf.java
@@ -14,35 +14,117 @@ import java.io.*;
 
 public class Wolf {
  
- boolean attack=false;
- private int x, y, vx, vy;
- private final int halfsize = 10;
- 
- public int getX(){
-  return x;
- }
- public int getY(){
-  return y;
- }
+	boolean attack=false;
+	private int x, y;
+	
+	private double dx, dy, vx, vy;
+	private final int halfsize = 10;
+ 	private final double a = 0.15; //accellaration is high, but slowdown will also be high
+ 	
+ 	
+	public int getX(){
+		return x;
+	}
+	public int getY(){
+		return y;
+	}
  
  
  
     public Wolf(int X, int Y) {
-     x=X;
-     y=Y;
-     
+    	x=X;
+    	y=Y;
+    	dx = (double)x;
+    	dy = (double)y;
+    	
+     	vx = 0;
+     	vy = 0;
+     	
     }
-    public void doMovement(int px,int py,int screenx,int screeny){
-      
+    
+    
+    
+    
+    
+    private final int aggroDist = 400;
+    private final double maxS = 8.0;
+    public void doMovement(int px,int py){
+    	
+    	//only speed u[ if in range
+    	if (Math.hypot(px - x, py - y) < aggroDist){
+    	
+	    	double newx, newy;
+	    	
+	    	//x velocity change
+	    	if (x < px){
+	    		vx += a;
+	    	}
+	    	else{
+	    		vx -= a;
+	    	}
+	    	
+	    	//y velocity cange
+	    	if (y < py){
+	    		vy += a;
+	    	}
+	    	else{
+	    		vy -= a;
+	    	}
+	    	
+	    	if (Math.abs(vx) > maxS){
+	    		if (vx < 0){
+	    			vx = -1*maxS;
+	    		}
+	    		else{
+	    			vx = maxS;
+	    		}
+	    	}
+	    	
+	    	if (Math.abs(vy) > maxS){
+	    		if (vy < 0){
+	    			vy = -1*maxS;
+	    		}
+	    		else{
+	    			vy = maxS;
+	    		}
+	    	}
+	    	
+	
+    	}
+    	//slow down if not in range
+    	else{
+    		slowDown();
+    	}
+    	
+    	dx += vx;
+	    dy += vy;
+    	
+    	x = (int)dx;
+    	y = (int)dy;
+    	
     }
+    
+    private void slowDown(){
+    	vx *= 0.8;
+    	vy *= 0.8;
+    	
+    	if (Math.abs(vx) < 0.001){
+    		vx = 0;
+    	}
+    	if (Math.abs(vy) < 0.001){
+    		vy = 0;
+    	}
+    }
+    
+    
     public Rectangle WolfBox(){
-     return new Rectangle(x - halfsize, y - halfsize, 2*halfsize, 2*halfsize); //can be changed accordingly 
+    	return new Rectangle(x - halfsize, y - halfsize, 2*halfsize, 2*halfsize); //can be changed accordingly 
     }
     public void damage(){
-      return;
+    	return;
     }
     public void draw(Graphics2D g,int screenx,int screeny){
-      g.setColor(Color.GREEN);
-      g.fillRect(x-halfsize-screenx,y-halfsize-screeny,2*halfsize,2*halfsize);
+    	g.setColor(Color.GREEN);
+    	g.fillRect(x-halfsize-screenx,y-halfsize-screeny,2*halfsize,2*halfsize);
     }
 }
